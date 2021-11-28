be3004968cf49b47bbe902260b48a24d78748198
kimchangmo <87592964+kimchangmo@users.noreply.github.com>
2021-11-28 14:55:20 +0900

    Update kcm_new.py

--- kcm_new.py
+++ kcm_new.py
@@ -264,7 +264,8 @@ while True:
                     web1_3 = 'false'
                     web1_4 = 'false'
                     web1_5 = 'false'
-                elif (count1 == 'false') and (rsi < 70) and (70 < oldrsi) and ((water_buy_price_0) < (get_current_price(buycoin_0))) :
+                #elif (count1 == 'false') and (rsi < 70) and (70 < oldrsi) and ((water_buy_price_0) < (get_current_price(buycoin_0))) :
+                elif (count1 == 'false') and (oldrsi < 70) and (70 < old_old_rsi) and ((water_buy_price_0) < (get_current_price(buycoin_0))) :
                     btc_0 = upbit.get_balance(buycoin_0[4:])
                     upbit.sell_market_order(buycoin_0, btc_0)
                     count1 = 'true'
@@ -380,7 +381,8 @@ while True:
                     web2_3 = 'false'
                     web2_4 = 'false'
                     web2_5 = 'false'
-                elif (count2 == 'false') and (rsi < 70) and (70 < oldrsi) and ((water_buy_price_1) < (get_current_price(buycoin_1))) :
+                #elif (count2 == 'false') and (rsi < 70) and (70 < oldrsi) and ((water_buy_price_1) < (get_current_price(buycoin_1))) :
+                elif (count2 == 'false') and (oldrsi < 70) and (70 < old_old_rsi) and ((water_buy_price_1) < (get_current_price(buycoin_1))) :
                     btc_1 = upbit.get_balance(buycoin_1[4:])
                     upbit.sell_market_order(buycoin_1, btc_1)
                     count2 = 'true'
@@ -496,7 +498,8 @@ while True:
                     web3_3 = 'false'
                     web3_4 = 'false'
                     web3_5 = 'false'
-                elif (count3 == 'false') and (rsi < 70) and (70 < oldrsi) and ((water_buy_price_2) < (get_current_price(buycoin_2))) :
+                #elif (count3 == 'false') and (rsi < 70) and (70 < oldrsi) and ((water_buy_price_2) < (get_current_price(buycoin_2))) :
+                elif (count3 == 'false') and (oldrsi < 70) and (70 < old_old_rsi) and ((water_buy_price_2) < (get_current_price(buycoin_2))) :
                     btc_2 = upbit.get_balance(buycoin_2[4:])
                     upbit.sell_market_order(buycoin_2, btc_2)
                     count3 = 'true'
