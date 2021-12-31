a7d383a75db49a09d5cd3dae55313486ccac7d08
kimchangmo <87592964+kimchangmo@users.noreply.github.com>
2021-12-31 23:29:44 +0900

    Update kcm_1minite.py

--- kcm_1minite.py
+++ kcm_1minite.py
@@ -32,6 +32,7 @@ def get_current_price(ticker):
     """현재가 조회"""
     return pyupbit.get_orderbook(tickers=ticker)[0]["orderbook_units"][0]["ask_price"]
 
+#이평선 
 now_ma20 = 0
 now_ma60 = 0
 old_ma20 = 0
@@ -56,6 +57,54 @@ def get_ma(ticker):
     old_ma20 =  round(ma20.iloc[-2],2)
     old_ma60 =  round(ma60.iloc[-2],2)
 
+#rsi, 볼밴 조회
+rsi = 0
+oldrsi = 0
+old_old_rsi = 0
+band_high = 0
+band_low = 0
+def rsiindex(symbol):
+    global rsi
+    global oldrsi
+    global old_old_rsi
+    global band_high
+    global band_low
+    url = "https://api.upbit.com/v1/candles/minutes/1"
+    querystring = {"market":symbol,"count":"500"}
+    response = requests.request("GET", url, params=querystring)
+    data = response.json()
+    df = pd.DataFrame(data)
+    #여기서부터 볼밴 상/하단값 구하는법
+    df_bol=df['trade_price'].iloc[::-1]
+    unit=2
+    band1=unit*numpy.std(df_bol[len(df_bol)-20:len(df_bol)])
+    bb_center=numpy.mean(df_bol[len(df_bol)-20:len(df_bol)])
+    band_high = bb_center+band1
+    band_high = round(band_high,2)
+    band_low = bb_center-band1
+    band_low = round(band_low,2)
+    #여기까지
+    df=df.reindex(index=df.index[::-1]).reset_index()
+    old = df['index'] > 0
+    old_old = df['index'] > 1
+    
+    def rsi(ohlc: pd.DataFrame, period: int = 14):
+        ohlc["trade_price"] = ohlc["trade_price"]
+        delta = ohlc["trade_price"].diff()
+        gains, declines = delta.copy(), delta.copy()
+        gains[gains < 0] = 0
+        declines[declines > 0] = 0
+
+        _gain = gains.ewm(com=(period - 1), min_periods=period).mean()
+        _loss = declines.abs().ewm(com=(period - 1), min_periods=period).mean()
+
+        RS = _gain / _loss
+        return pd.Series(100 - (100 / (1 + RS)), name="RSI")
+
+    oldrsi = rsi(df[old], 14).iloc[-1]
+    old_old_rsi = rsi(df[old_old], 14).iloc[-1]
+    rsi = rsi(df, 14).iloc[-1]
+    
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
@@ -68,7 +117,7 @@ all_coin.remove('KRW-ETH')
 #총 몇개 돌릴건지 설정
 coin_buy_index = 6
 #분봉 +1
-delay_time = 16
+delay_time = 1
 #구매가
 buy_money = 100000
 
@@ -105,8 +154,22 @@ while True:
                 #print(now_ma60)
                 #print(old_ma20)
                 #print(old_ma60)
-                #이전,이이전 비교문
-                if (old_ma20 < old_ma60) and (now_ma20 > now_ma60) and (current_price > 3000) and (count_all == 'true') and (upbit.get_balance(coin[4:]) == 0):
+                
+                #코인 전전가격 체크
+                url = "https://api.upbit.com/v1/candles/minutes/1"
+                querystring = {"market":coin,"count":"200"}
+                response = requests.request("GET", url, params=querystring)
+                data = response.json()
+                df = pd.DataFrame(data)
+                df=df['trade_price'].iloc[::-1]
+                #print(coin, ":" , df)
+                old_old_price = df[2]
+                #print("old_old:" , old_old_price)
+                
+                #이평선 매수
+                #if (old_ma20 < old_ma60) and (now_ma20 > now_ma60) and (current_price > 3000) and (count_all == 'true') and (upbit.get_balance(coin[4:]) == 0):
+                #rsi 매수
+                if (30 > old_old_rsi) and (30 < oldrsi) and (count_all == 'true') and (upbit.get_balance(coin[4:]) == 0):
                 #test
                 #if (current_price > 3000) and (count_all == 'true') and (upbit.get_balance(coin[4:]) == 0):
                     for i in range(0, coin_buy_index):
@@ -119,12 +182,16 @@ while True:
                                 globals()['water_buy_price_{}'.format(i)] = current_price
                                 print("buy:",  globals()['buycoin_{}'.format(i)])
                                 globals()['count_{}'.format(i)] = 'false'
+                                globals()['old_old_price_{}'.format(i)] = old_old_price
                                 time.sleep(5)
                             break
                 
                 for i in range(0, coin_buy_index):  
-                    if (globals()['count_{}'.format(i)] == 'false'):
-                        get_ma(globals()['buycoin_{}'.format(i)])
+                    #if (globals()['count_{}'.format(i)] == 'false'):
+                    #    get_ma(globals()['buycoin_{}'.format(i)])
+                    #rsi 값 구하기
+                    if (globals()['count_{}'.format(i)] == 'false') :
+                        rsiindex(globals()['buycoin_{}'.format(i)])
 
                     #0.3퍼 판매
                     if (globals()['count_{}'.format(i)] == 'false') and ((globals()['water_buy_price_{}'.format(i)] * 1.003) < (get_current_price(globals()['buycoin_{}'.format(i)]))) :
@@ -135,7 +202,7 @@ while True:
                         #count1 = 'true'
 
                     #손절
-                    if (globals()['count_{}'.format(i)] == 'false') and (now_ma20 < now_ma60):
+                    if (globals()['count_{}'.format(i)] == 'false') and (globals()['old_old_price_{}'.format(i)] > get_current_price(globals()['buycoin_{}'.format(i)])):
                         globals()['btc_{}'.format(i)] = upbit.get_balance(globals()['buycoin_{}'.format(i)][4:])
                         #btc_0 = upbit.get_balance(buycoin_0[4:])
                         upbit.sell_market_order(globals()['buycoin_{}'.format(i)], globals()['btc_{}'.format(i)])
@@ -148,8 +215,11 @@ while True:
 
             else:
                 for i in range(0, coin_buy_index):
-                    if (globals()['count_{}'.format(i)] == 'false'):
-                        get_ma(globals()['buycoin_{}'.format(i)])
+                    #if (globals()['count_{}'.format(i)] == 'false'):
+                    #    get_ma(globals()['buycoin_{}'.format(i)])
+                    #rsi 값 구하기
+                    if (globals()['count_{}'.format(i)] == 'false') :
+                        rsiindex(globals()['buycoin_{}'.format(i)])
 
                     #0.3퍼 판매
                     if (globals()['count_{}'.format(i)] == 'false') and ((globals()['water_buy_price_{}'.format(i)] * 1.003) < (get_current_price(globals()['buycoin_{}'.format(i)]))) :
@@ -160,7 +230,7 @@ while True:
                         #count1 = 'true'
 
                     #손절
-                    if (globals()['count_{}'.format(i)] == 'false') and (now_ma20 < now_ma60):
+                    if (globals()['count_{}'.format(i)] == 'false') and (globals()['old_old_price_{}'.format(i)] > get_current_price(globals()['buycoin_{}'.format(i)])):
                         globals()['btc_{}'.format(i)] = upbit.get_balance(globals()['buycoin_{}'.format(i)][4:])
                         #btc_0 = upbit.get_balance(buycoin_0[4:])
                         upbit.sell_market_order(globals()['buycoin_{}'.format(i)], globals()['btc_{}'.format(i)])
