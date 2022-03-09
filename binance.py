3b5ecaf956dd3adb1bbd4ee6df23559e0ed9fece
kimchangmo <87592964+kimchangmo@users.noreply.github.com>
2022-03-10 02:04:13 +0900

    Update binance.py

--- binance.py
+++ binance.py
@@ -11,7 +11,7 @@
 
 #시계 자동 동기화 적용 할것!!!
 
------------테스트코드--------------------------------------------------------------------------
+#-----------테스트코드--------------------------------------------------------------------------
 import ccxt 
 import pprint
 import time
@@ -152,7 +152,7 @@ coin_buy_index = 1
 #분봉 +1
 delay_time = 17
 #구매가
-buy_money = 0.05 #0.05 = 5.34USDT(LTC기준)
+buy_money = 0.24 #레버리지 3배면 넣고싶은 금액의 3배 넣어야함
 
 coin_one_buy = 'true'
 coin_one_sell = 'true'
@@ -169,19 +169,6 @@ while True:
             now_rsi = float(rsi(coin).iloc[-1])
             old_rsi = float(rsi(coin).iloc[-2])
             old_old_rsi = float(rsi(coin).iloc[-3])
-
-            #시간동기화
-            #client = r_Client(api_key=api_key, api_secret=secret)
-            #gt = client.get_server_time()
-            #aa = str(gt)
-            #bb = aa.replace("{'serverTime': ","")
-            #aa = bb.replace("}","")
-            #gg=int(aa)
-            #ff=gg-10799260
-            #uu=ff/1000
-            #yy=int(uu)
-            #tt=time.localtime(yy)
-            #win32api.SetSystemTime(tt[0],tt[1],0,tt[2],tt[3],tt[4],tt[5],0)
 
             now = dt.datetime.now()
             #print(now)
@@ -206,10 +193,11 @@ while True:
                     price_buy = client.futures_symbol_ticker(symbol=coin)
                     water_buy_price_buy = price_buy['price']
                     all_purchase_volume_buy_buy = price_buy['price']
-                    last_current_price_buy = price_buy['price']
-                    print('코인(롱) 현재가:', water_buy_price_buy)
-                    print('코인(롱) 물타기가:', float(water_buy_price_buy) * 0.97)
-                    print('코인(롱) 익절가:', float(water_buy_price_buy) * 1.03)
+                    last_current_price_buy = float(price_buy['price'])
+                    print('코인(롱) 구매가:', water_buy_price_buy)
+                    print('')
+                    #print('코인(롱) 물타기가:', float(water_buy_price_buy) * 0.98)
+                    #print('코인(롱) 익절가:', float(water_buy_price_buy) * 1.02)
 
                     #구매시간
                     buytime_buy = dt.datetime.now() + dt.timedelta(minutes=delay_time)
@@ -224,7 +212,7 @@ while True:
                         positionSide = 'LONG', type='MARKET', quantity=buy_money
                     ) #0.05 = 5.34USDT
     
-                    print('구매(롱)')
+                    #print('구매(롱)')
                     coin_one_buy = 'false'
                     time.sleep(1)    
 
@@ -240,9 +228,10 @@ while True:
                     water_buy_price_sell = price_sell['price']
                     all_purchase_volume_buy_sell = price_sell['price']
                     last_current_price_sell = price_sell['price']
-                    print('코인(숏) 현재가:', water_buy_price_sell)
-                    print('코인(숏) 물타기가:', float(water_buy_price_sell) * 1.03)
-                    print('코인(숏) 익절가:', float(water_buy_price_sell) * 0.97)
+                    print('코인(숏) 구매가:', water_buy_price_sell)
+                    print('')
+                    #print('코인(숏) 물타기가:', float(water_buy_price_sell) * 1.02)
+                    #print('코인(숏) 익절가:', float(water_buy_price_sell) * 0.98)
 
                     #구매시간
                     buytime_sell = dt.datetime.now() + dt.timedelta(minutes=delay_time)
@@ -257,7 +246,7 @@ while True:
                         positionSide = 'SHORT', type='MARKET', quantity=buy_money
                     )
 
-                    print('구매(숏)')
+                    #print('구매(숏)')
                     coin_one_sell = 'false'
                     time.sleep(1)    
 
@@ -290,9 +279,10 @@ while True:
                 )
 
                 print('익절(롱)')
+                print('')
                 
                 coin_one_buy = 'true'
-                time.sleep(10)    
+                time.sleep(1)    
 
             #2퍼 익절(숏)
             if (coin_one_sell == 'false') and ((float(water_buy_price_sell) * 0.98) > current_price_sell) :
@@ -304,12 +294,63 @@ while True:
                     positionSide = 'SHORT', type='MARKET', quantity=old_plus_sell
                 )
                 print('익절(숏)')
+                print('')
                 
                 coin_one_sell = 'true'
                 time.sleep(1)    
 
             #물타기(롱)
             if (coin_one_buy == 'false') and (30 > old_old_rsi) and (30 < old_rsi) and (30 < now_rsi) and (now > buytime_buy) and ((float(last_current_price_buy) * 0.97) > current_price_buy):
+            #if (coin_one_buy == 'false'):
+                #선물잔고조회
+                balance = binance.fetch_balance(params={"type": "future"})
+                #balance = binance.fetch_balance()
+                #print(balance['USDT']['used'])
+                
+                if (float(buy_money_buy)*2 < balance['USDT']['free']) :
+                    #레버리지 설정
+                    client.futures_change_leverage(symbol = coin, leverage = 3)
+                    #구매
+                    client.futures_create_order(
+                        symbol=coin, side='BUY',
+                        positionSide = 'LONG', type='MARKET', quantity=buy_money_buy*2
+                    ) #0.05 = 5.34USDT
+
+                    #구매시간 갱신
+                    buytime_buy = dt.datetime.now() + dt.timedelta(minutes=delay_time)
+                    #물타기가격 갱신(2배)
+                    old_buy_money_buy = buy_money_buy
+                    buy_money_buy = float(buy_money_buy)*2
+                    
+                    current_price_buy = client.futures_symbol_ticker(symbol=coin)
+                    current_price_buy = float(current_price_buy['price'])
+                    last_current_price_buy = current_price_buy
+                    
+                    #총 매수량 : all_purchase_volume_buy
+                    if water_buy_price_buy == price_buy['price'] :
+                        all_purchase_volume_buy = (float(water_buy_price_buy) * float(old_buy_money_buy)) + (float(current_price_buy) * float(buy_money_buy))
+                    else :
+                        all_purchase_volume_buy = (float(water_buy_price_buy) * float(old_plus_buy)) + (float(current_price_buy) * float(buy_money_buy))
+                    
+                    #총 비용 : old_plus_buy
+                    if old_buy_money_buy == buy_money :
+                        old_plus_buy = old_buy_money_buy + buy_money_buy
+                    else :
+                        old_plus_buy = old_plus_buy + buy_money_buy
+
+                    #총 매수량
+                    water_buy_price_buy = float(all_purchase_volume_buy)/float(old_plus_buy)
+                    print('old_old_rsi(롱)', old_old_rsi)
+                    print('old_rsi(롱)', old_rsi)
+                    print('물타기(롱)')
+                    print('물탄평단(롱)', water_buy_price_buy)
+                    print('물탄익절가(롱)', float(water_buy_price_buy)*1.02)
+                    print('')
+
+                    time.sleep(1)    
+
+            #배율에따른 청산방지(롱)
+            if (coin_one_buy == 'false') and ((float(water_buy_price_buy) * 0.72) > current_price_buy):
             #if (coin_one_buy == 'false'):
                 #선물잔고조회
                 balance = binance.fetch_balance(params={"type": "future"})
@@ -333,7 +374,7 @@ while True:
                     
                     current_price_buy = client.futures_symbol_ticker(symbol=coin)
                     current_price_buy = float(current_price_buy['price'])
-                    last_current_price_buy = float(current_price_buy['price'])
+                    last_current_price_buy = current_price_buy
                     
                     #총 매수량 : all_purchase_volume_buy
                     if water_buy_price_buy == price_buy['price'] :
@@ -348,14 +389,15 @@ while True:
                         old_plus_buy = old_plus_buy + buy_money_buy
 
                     #총 매수량
-                    water_buy_price_buy = all_purchase_volume_buy/old_plus_buy
-                    print('old_old_rsi(롱)', old_old_rsi)
-                    print('old_rsi(롱)', old_rsi)
-                    print('물타기(롱)')
+                    water_buy_price_buy = float(all_purchase_volume_buy)/float(old_plus_buy)
+                    #print('old_old_rsi(롱)', old_old_rsi)
+                    #print('old_rsi(롱)', old_rsi)
+                    print('청산방지(롱)')
                     print('물탄평단(롱)', water_buy_price_buy)
-                    print('물탄익절가(롱)', float(water_buy_price_buy)*1.03)
+                    print('물탄익절가(롱)', float(water_buy_price_buy)*1.02)
+                    print('')
 
-                    time.sleep(1)    
+                    time.sleep(1) 
 
             #물타기(숏)
             if (coin_one_sell == 'false') and (70 < old_old_rsi) and (70 > old_rsi) and (70 > now_rsi) and (now > buytime_sell) and ((float(last_current_price_sell) * 1.03) < current_price_sell):
@@ -382,7 +424,7 @@ while True:
                     
                     current_price_sell = client.futures_symbol_ticker(symbol=coin)
                     current_price_sell = float(current_price_sell['price'])
-                    last_current_price_sell = float(current_price_sell['price'])
+                    last_current_price_sell = current_price_sell
                     
                     #총 매수량 : all_purchase_volume_sell
                     if water_buy_price_sell == price_sell['price'] :
@@ -397,14 +439,65 @@ while True:
                         old_plus_sell = old_plus_sell + buy_money_sell
 
                     #총 매수량
-                    water_buy_price_sell = all_purchase_volume_sell/old_plus_sell
+                    water_buy_price_sell = float(all_purchase_volume_sell)/float(old_plus_sell)
                     print('old_old_rsi(숏)', old_old_rsi)
                     print('old_rsi(숏)', old_rsi)
                     print('물타기(숏)')
                     print('물탄평단(숏)', water_buy_price_sell)
-                    print('물탄익절가(숏)', float(water_buy_price_sell)*0.97)
+                    print('물탄익절가(숏)', float(water_buy_price_sell)*0.98)
+                    print('')
 
-                    time.sleep(1)      
+                    time.sleep(1)    
+
+            #배율에따른 청산방지(숏)
+            if (coin_one_sell == 'false') and ((float(water_buy_price_sell) * 1.28) < current_price_sell):
+                #선물잔고조회
+                balance = binance.fetch_balance(params={"type": "future"})
+                #balance = binance.fetch_balance()
+                #print(balance['USDT']['used'])
+                
+                if (buy_money_sell*2 < balance['USDT']['free']) :
+                    #레버리지 설정
+                    client.futures_change_leverage(symbol = coin, leverage = 3)
+                    #구매
+                    #upbit.buy_market_order(globals()['buycoin_{}'.format(i)], globals()['buy_money_{}'.format(i)]*2)
+                    client.futures_create_order(
+                        symbol=coin, side='SELL',
+                        positionSide = 'SHORT', type='MARKET', quantity=buy_money_sell*2
+                    ) #0.05 = 5.34USDT
+
+                    #구매시간 갱신
+                    buytime_sell = dt.datetime.now() + dt.timedelta(minutes=delay_time)
+                    #물타기가격 갱신(2배)
+                    old_buy_money_sell = buy_money_sell
+                    buy_money_sell = buy_money_sell*2
+                    
+                    current_price_sell = client.futures_symbol_ticker(symbol=coin)
+                    current_price_sell = float(current_price_sell['price'])
+                    last_current_price_sell = current_price_sell
+                    
+                    #총 매수량 : all_purchase_volume_sell
+                    if water_buy_price_sell == price_sell['price'] :
+                        all_purchase_volume_sell = (float(water_buy_price_sell) * float(old_buy_money_sell)) + (float(current_price_sell) * float(buy_money_sell))
+                    else :
+                        all_purchase_volume_sell = (float(water_buy_price_sell) * float(old_plus_sell)) + (float(current_price_sell) * float(buy_money_sell))
+                    
+                    #총 비용 : old_plus_sell
+                    if old_buy_money_sell == buy_money :
+                        old_plus_sell = old_buy_money_sell + buy_money_sell
+                    else :
+                        old_plus_sell = old_plus_sell + buy_money_sell
+
+                    #총 매수량
+                    water_buy_price_sell = float(all_purchase_volume_sell)/float(old_plus_sell)
+                    #print('old_old_rsi(숏)', old_old_rsi)
+                    #print('old_rsi(숏)', old_rsi)
+                    print('청산방지(숏)')
+                    print('물탄평단(숏)', water_buy_price_sell)
+                    print('물탄익절가(숏)', float(water_buy_price_sell)*0.98)
+                    print('')
+
+                    time.sleep(1)    
 
             
             n = n+1
